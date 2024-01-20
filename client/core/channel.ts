b9446561812c2ebb4c954a700575619de09415bc
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2024-01-20 11:15:49 -0800

    Complete Channel factory and display channel name and last message in sidebar.

--- client/core/channel.ts
+++ client/core/channel.ts
@@ -6,7 +6,8 @@ type User = {
 type Channel = GroupChannel | DirectChannel;
 
 export function ChannelFactory(data: unknown): Channel | { type: "None" } {
-  if (["direct", "group"].includes(data?.channel_type)) return { type: "None" };
+  // console.log(JSON.stringify(data));
+  if (!["direct", "group"].includes(data?.channel_type)) return { type: "None" };
 
   if (data.channel_type === "group") return GroupChannel(data);
 
@@ -25,6 +26,12 @@ type GroupChannel = {
 function GroupChannel(data: unknown): GroupChannel {
   return {
     type: "GroupChannel",
+    id: data?.channel_id, 
+    channel_type: "group",
+    group_name: data?.group_name,
+    users: JSON.parse(data?.users),
+    last_message: data?.last_message, 
+    timestamp: new Date((data?.timestamp?.seconds || 0) * 1000),
   };
 }
 
@@ -38,10 +45,10 @@ export type DirectChannel = {
 
 function DirectChannel(data: unknown): DirectChannel {
   return {
-    id: data.id,
-    timestamp: new Date(data.timestamp),
+    id: data?.channel_id,
+    timestamp: new Date((data?.timestamp?.seconds || 0) * 1000),
     channel_type: "direct",
     last_message: data?.last_message,
-    users: data?.users ?? [],
+    users: JSON.parse(data?.users),
   };
 }
